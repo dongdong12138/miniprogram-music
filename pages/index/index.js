301486780267ff68941c61354f34df46f5ead8f1
BarryDong <286084845@qq.com>
2024-01-03 16:11:50 +0800

    获取推荐歌单数据 & 封装 NavHeader 组件

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,15 +11,23 @@ Page({
 		iconList: [
 			{icon: 'icon-meirituijian', text: '每日推荐'}, {icon: 'icon-gedan1', text: '歌单'}, {icon: 'icon-icon-ranking', text: '排行榜'},
 			{icon: 'icon-diantai', text: '电台'}, {icon: 'icon-zhiboguankanliangbofangsheyingshexiangjixianxing', text: '直播'},
-		]
+		],
+		recommendList: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: async function (options) {
+		// 获取轮播数据
 		let bannerListData = await request('/banner', {type: 2})
-		this.setData({ bannerList: bannerListData.banners })
+		// 获取推荐歌单数据
+		let recommendListData = await request('/personalized', {limit: 10})
+		
+    this.setData({
+			bannerList: bannerListData.banners,
+      recommendList: recommendListData.result
+    })
   },
 
   /**
